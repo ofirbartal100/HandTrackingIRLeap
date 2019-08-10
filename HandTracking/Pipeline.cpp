125d8fe22b63dfa6717d47c5de989c8563d1d376
Ofir Bartal <ofirbartal100@gmail.com>
2019-08-10 17:59:13 +0300

    Using Basler API to capture 500fps video, showing preview, and saving to .avi file. Working.

--- HandTracking/Pipeline.cpp
+++ HandTracking/Pipeline.cpp
@@ -6,7 +6,9 @@
 #include "opencv2/core.hpp"
 #include <opencv2/objdetect.hpp>
 #include <ctime>
-
+#include <ppl.h>
+#include <concurrent_queue.h>
+#include <thread>
 // Include files to use the pylon API.
 #include <pylon/PylonIncludes.h>
 #include <pylon/usb/BaslerUsbInstantCamera.h>
@@ -14,310 +16,213 @@
 #    include <pylon/PylonGUI.h>
 #endif
 
-#define FREQUENCY_OF_ROBUST_TRACKER 10
-#define HIGHFPS 500
-#define DEBUG false
-
-// Convert to string
-#define SSTR( x ) static_cast< std::ostringstream & >( \
-( std::ostringstream() << std::dec << x ) ).str()
-
 using namespace cv;
 using namespace std;
 using namespace Pylon;
-
-Rect2d getNeighborsROI(const Rect2d& rect, const Mat& mat, float neighboring_scale);
-
-Mat UpdateROI(float neighboringScale, Mat frame, Rect2d bbox);
+using namespace concurrency;
 
 
-DWORD WINAPI myThread(LPVOID lpParameter)
+class BaslerCamera
 {
-	unsigned int& myCounter = *((unsigned int*)lpParameter);
-	while (myCounter < 0xFFFFFFFF) ++myCounter;
-	return 0;
-}
-
-int main(int argc, char **argv)
-{
-
-#pragma region Tracker Initializations
-
-
-	// List of tracker types in OpenCV 3.4.1
-	string trackerWorkingTypes[5] = { "KCF", "TLD","MEDIANFLOW", "MOSSE", "CSRT" };
-	string trackerNotWorkingTypes[8] = { "BOOSTING", "MIL", "GOTURN" };
-
-	// Create a tracker
-	string trackerType = trackerWorkingTypes[2];
-
-	Ptr<Tracker> tracker;
-
-	if (trackerType == "BOOSTING")
-	{
-		TrackerBoosting::Params params;
-		tracker = TrackerBoosting::create(params);
-	}
-	if (trackerType == "MIL")
-	{
-		TrackerMIL::Params params;
-		params.featureSetNumFeatures = 250;
-		params.samplerTrackMaxNegNum = 100;
-		params.samplerTrackMaxPosNum = 100000;
-		params.samplerTrackInRadius = 4;
-		params.samplerSearchWinSize = 65;
-		params.samplerInitMaxNegNum = 25;
-		params.samplerInitInRadius = 3;
-		tracker = TrackerMIL::create(params);
-	}
-	if (trackerType == "KCF")
-		tracker = TrackerKCF::create();
-	if (trackerType == "TLD")
-		tracker = TrackerTLD::create();
-	if (trackerType == "MEDIANFLOW")
-		tracker = TrackerMedianFlow::create();
-	if (trackerType == "GOTURN")
-		tracker = TrackerGOTURN::create();
-	if (trackerType == "MOSSE")
-		tracker = TrackerMOSSE::create();
-	if (trackerType == "CSRT")
-		tracker = TrackerCSRT::create();
-
-
-	//ROI Weights Initialization
-	float neighboringScale = 0.05;
-
-	//HaarCascade Detector
-	/*CascadeClassifier handDetector;
-	if (!handDetector.load("C:\\Users\\ofir\\Desktop\\temp\\palm_v4.xml")) {
-		std::cout << "Error Loading Cascade" << endl;
-		return 1;
-	}
-*/
-#pragma endregion
-
-#pragma region Capture Initializations
-	PylonInitialize();
-
-	// Create an instant camera object with the camera device found first.
-	CBaslerUsbInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
-
-	// Print the model name of the camera.
-	cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
-
+private:
+	// Camera instance pointer
+	CBaslerUsbInstantCamera * camera;
 
 	// This smart pointer will receive the grab result data.
 	CGrabResultPtr ptrGrabResult;
-	
-	camera.Open();
-	camera.AcquisitionFrameRateEnable = true;
-	camera.AcquisitionFrameRate = 500;
-
-
-
-
-	
-#pragma endregion
-
-#pragma region BoundingBox Initialization
-	// Read first frame 
-	Mat frame;
-	camera.GrabOne(5000, ptrGrabResult, TimeoutHandling_ThrowException);
-	if (ptrGrabResult->GrabSucceeded())
+public:
+	BaslerCamera()
 	{
-		frame = Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8U, (uint8_t*)ptrGrabResult->GetBuffer());
+		PylonInitialize();
+
+		// Create an instant camera object with the camera device found first.
+		camera = new CBaslerUsbInstantCamera(CTlFactory::GetInstance().CreateFirstDevice());
+
+		// Print the model name of the camera.
+		cout << "Using device " << camera->GetDeviceInfo().GetModelName() << endl;
+
+		camera->Open();
+		camera->AcquisitionFrameRateEnable = true;
+		camera->AcquisitionFrameRate = 500;
+		camera->ExposureTime = 1000;
 	}
-	// select a bounding box 
-	//Rect2d bbox = selectROI(frame, false);
-#pragma endregion
-
-#pragma region Update ROI
-	//UpdateROI(neighboringScale, frame, bbox);
-#pragma endregion
-
-#pragma region Show Image
-	if (DEBUG)
+	~BaslerCamera()
 	{
-		// Display bounding box. 
-//		rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+		Close();
 	}
 
-	cv::imshow("Tracking", frame);
-#pragma endregion
-
-
-#pragma region Video Writer
-	/*const string& outputName = "outputVideo.avi";
-	Size frameSize(frame.cols, frame.rows);
-	VideoWriter outputVideo;
-	outputVideo.open(outputName, VideoWriter::fourcc('M', 'J', 'P', 'G'), 500, frameSize,false);
-*/
-#pragma endregion 
-
-#pragma region Reading Loop
-	int updateFromRobustTracker = HIGHFPS / FREQUENCY_OF_ROBUST_TRACKER;
-	//tracker->init(frame, bbox);
-
-
-	// The camera device is parameterized with a default configuration which
-	// sets up free-running continuous acquisition.
-	camera.StartGrabbing();
-	int counter = 0;
-	camera.ExposureTime = 1000;
-	double rrrrr2 = camera.ExposureTime.GetValue();
-	double rrrrr = camera.ResultingFrameRate.GetValue();
-	// Camera.StopGrabbing() is called automatically by the RetrieveResult() method
-	clock_t begin = clock();
-	cout << "start";
-	while (camera.IsGrabbing()&&counter++ < 10000)
+	void StartGrabbing() const
 	{
-
-		// Wait for an image and then retrieve it. A timeout of 5000 ms is used.
-		camera.RetrieveResult(500, ptrGrabResult, TimeoutHandling_ThrowException);
+		camera->StartGrabbing();
+	}
+	Mat RetrieveFrame()
+	{
+		// Camera.StopGrabbing() is called automatically by the RetrieveResult() method
+		// Wait for an image and then retrieve it. A timeout of 500 ms is used.
+		camera->RetrieveResult(500, ptrGrabResult, TimeoutHandling_ThrowException);
 		// Image grabbed successfully?
 		if (ptrGrabResult->GrabSucceeded())
 		{
-			frame = Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8U, (uint8_t*)ptrGrabResult->GetBuffer());
-			//#pragma region Robust Tracker Update Region
-			//
-			//
-			//			if (updateFromRobustTracker == 0)
-			//			{
-			//#pragma region Robust Tracker Update
-			//				//select a bounding box 
-			//				bbox = selectROI(frame, false);
-			//				tracker.release();
-			//				tracker = TrackerMedianFlow::create();
-			//				tracker->init(frame, bbox);
-			//				updateFromRobustTracker = HIGHFPS / FREQUENCY_OF_ROBUST_TRACKER;
-			//#pragma endregion
-			//			}
-			//			updateFromRobustTracker--;
-			//
-			//#pragma endregion
-			//
-			//#pragma region Fast Tracker Update
-			//			// Start timer
-			//			double timer = (double)getTickCount();
-			//
-			//			// Update the tracking result
-			//			bool ok = tracker->update(frame, bbox);
-			//
-			//#pragma region Haar Cascade Detector
-			//
-			//			std::vector<Rect> hands;
-			//			//update search ROI
-			//			Rect2d neighboringROI = getNeighborsROI(bbox, frame, neighboringScale);
-			//
-			//			const Mat ROIref = frame(neighboringROI);
-			//			Mat MaxROI;
-			//			ROIref.copyTo(MaxROI);
-			//
-			//			cv::imshow("ROI", MaxROI);
-			//
-			//			Mat ROI_gray;
-			//			Size minSize(int(MaxROI.cols*(1 - neighboringScale)), int(MaxROI.rows*(1 - neighboringScale)));
-			//
-			//			/*cv::cvtColor(MaxROI, ROI_gray, CV_BGR2GRAY);
-			//			cv::equalizeHist(ROI_gray, ROI_gray);*/
-			//			//handDetector.detectMultiScale(ROI_gray, hands, 1 + 0.5*neighboringScale, 2, 0, minSize);
-			//			handDetector.detectMultiScale(MaxROI, hands, 1 + 0.5*neighboringScale, 2, 0, minSize);
-			//			if (hands.size() > 0)
-			//			{
-			//				bbox.x = neighboringROI.x + hands[0].x;
-			//				bbox.y = neighboringROI.y + hands[0].y;
-			//				bbox.width = hands[0].width;
-			//				bbox.height = hands[0].height;
-			//			}
-			//
-			//#pragma endregion
-			//
-			//
-			//			// Calculate Frames per second (FPS)
-			//			float fps = getTickFrequency() / ((double)getTickCount() - timer);
-			//#pragma endregion
-			//
+			//Return an Mat object containing the captured frame
+			return Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8U, (uint8_t*)ptrGrabResult->GetBuffer());
+		}
+		else
+		{
+			//Error Mat
+			return Mat();
+		}
+	}
+	bool IsGrabbing() const
+	{
+		return camera->IsGrabbing();
+	}
+	void Close() {
+		if (camera->IsOpen())
+			camera->Close();
+	}
+};
 
-#pragma region Show Image
-			//if (DEBUG)
-			//{
-			//	if (ok)
-			//	{
-			//		// Tracking success : Draw the tracked object
-			//		rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
-			//	}
-			//	else
-			//	{
-			//		// Tracking failure detected.
-			//		putText(frame, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2);
-			//	}
+class VideoSaver
+{
+private:
+	VideoWriter outputVideo;
+	concurrent_queue<Mat> grabbed_frames;
+	bool running;
 
-			//	// Display tracker type on frame
-			//	putText(frame, trackerType + " Tracker", Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+	int frameRate;
+	Size frameSize;
 
-			//	// Display FPS on frame
-			//	putText(frame, "FPS : " + SSTR(int(fps)), Point(100, 50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
-			//}
+	thread* saving_thread;
+	// Background thread function to asynchronicly save captured frames to a video file
+	void saving_loop()
+	{
+		Mat temp_frame;
 
-			// Save frame
-			//outputVideo.write(frame);
-			// Display frame.
-			cv::imshow("Tracking", frame);
-#pragma endregion
+		cout << "start saving video" << endl;
+		//As long as we are saving, and there are frames to save
+		while (running || !grabbed_frames.empty())
+		{
+			if (grabbed_frames.try_pop(temp_frame))
+			{
+				outputVideo << temp_frame;
+			}
+			Sleep(1);
+		}
+		cout << "done saving video" << endl;
+	}
+public:
+	VideoSaver()
+	{
+		running = false;
+		frameRate = 500;
+		frameSize = Size(720, 540);
+	}
+	~VideoSaver()
+	{
+		if (running)
+			Close();
+	}
 
-			// Exit if ESC pressed.
-			int k = waitKey(1);
+	//start the background thread that saves frames.
+	void Start(string outputName = "outputVideo.avi")
+	{
+		outputVideo.open(outputName, VideoWriter::fourcc('M', 'J', 'P', 'G'), frameRate, frameSize, false);
+		running = true;
+		saving_thread = new thread(&VideoSaver::saving_loop, this);
+	}
+
+	//add a frame to the saving frames queue if running
+	void AddFrame(Mat frame)
+	{
+		if (running)
+			grabbed_frames.push(frame);
+	}
+
+	//stop recieving new frames and join the background thread
+	void Close()
+	{
+		running = false;
+		saving_thread->join();
+		outputVideo.release();
+	}
+};
+
+class VideoShower
+{
+private:
+	const Mat* referencedFrame;
+	bool running;
+
+	thread* showing_thread;
+	void showing_loop()
+	{
+		cout << "start showing" << endl;
+
+		while (running)
+		{
+			//every loop takes the most up to date value of the referenced frame, and show it when ready
+			Mat temp = referencedFrame->clone();
+			cv::imshow("Preview", temp);
+			int k = waitKey(30);
 			if (k == 27)
 			{
 				break;
 			}
-
 		}
-		else
-		{
-			cout << "Error: " << ptrGrabResult->GetErrorCode() << " " << ptrGrabResult->GetErrorDescription() << endl;
-		}
-
+		cout << "done showing" << endl;
 	}
-
-	clock_t end = clock();
-	cout << "done in:" << double(end - begin)/CLOCKS_PER_SEC;
-#pragma endregion
-	//outputVideo.release();
-
-}
-
-Rect2d getNeighborsROI(const Rect2d& rect, const Mat& mat, float neighboring_scale)
-{
-	int width = rect.width * (1 + neighboring_scale);
-	int height = rect.height * (1 + neighboring_scale);
-	int x = rect.x - (width - rect.width) / 2;
-	int y = rect.y - (height - rect.height) / 2;
-
-	if (x < 0) x = 0;
-	if (y < 0) y = 0;
-
-	if (x + width >= mat.cols)
+public:
+	VideoShower() { running = false; }
+	~VideoShower()
 	{
-		width = mat.cols - x - 1;
+		if (running)
+			Stop();
 	}
-	if (y + height >= mat.rows)
+
+	//start the showing thread in the background, showing the registered frame
+	void Start(const Mat* registerFrame)
 	{
-		height = mat.rows - y - 1;
+		running = true;
+		referencedFrame = registerFrame;
+		showing_thread = new thread(&VideoShower::showing_loop, this);
 	}
 
-	return Rect2d(x, y, width, height);
-}
+	//stop the showing thread, waiting for it to finish
+	void Stop()
+	{
+		running = false;
+		showing_thread->join();
+	}
+};
 
 
-Mat UpdateROI(float neighboringScale, Mat frame, Rect2d bbox)
+int main(int argc, char **argv)
 {
-	//update search ROI
-	Rect2d neighboringROI = getNeighborsROI(bbox, frame, neighboringScale);
-	//extract the search roi out of the frame
-	const Mat ROIref = frame(neighboringROI);
-	Mat ROI;
-	ROIref.copyTo(ROI);
-	cv::imshow("ROI", ROI);
-	return ROI;
+	BaslerCamera basler_camera;
+	Mat frame;
+	VideoSaver videoSaver;
+	VideoShower video_shower;
+
+	int counter = 0;
+
+	//Initialize capture,saver,shower
+	basler_camera.StartGrabbing();
+	videoSaver.Start();
+	video_shower.Start(&frame);
+
+
+	//cout << "start capturing" << endl;
+	//clock_t begin = clock();
+	while (basler_camera.IsGrabbing() && counter++ < 500 * 5)
+	{
+		frame = basler_camera.RetrieveFrame();
+		videoSaver.AddFrame(frame.clone());
+	}
+	//clock_t end = clock();
+	//cout << "done capturing in: " << double(end - begin) / CLOCKS_PER_SEC << "seconds" << endl;
+
+	//Close all objects
+	video_shower.Stop();
+	videoSaver.Close();
+	basler_camera.Close();
 }
